7acdf0d9a5b7e192a862697149a1f10be4d577a9
Perciii <qs.sauvage@gmail.com>
2020-04-29 11:02:31 +0200

    tests classes skeleton

---
+++ src/test/java/RoueTest.java
@@ -0,0 +1,96 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class RoueTest {
+
+    /**
+     * Default constructor for test class src.VoitureTest
+     */
+    public RoueTest() {
+    }
+
+    /**
+     * Sets up the test fixture.
+     * <p>
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp() {
+
+    }
+
+    /**
+     * Tears down the test fixture.
+     * <p>
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown() {
+    }
+
+    // Tests getters
+    @Test
+    public void testGetVoiture() {
+        assertEquals();
+    }
+
+    @Test
+    public void testGetDiametre() {
+        assertEquals();
+    }
+
+    @Test
+    public void testGetPoids() {
+        assertEquals();
+    }
+
+    // Tests constructors
+    @Test
+    public void testNewRoue1() {
+        assertEquals();
+    }
+
+    @Test
+    public void testNewRoue2() {
+        assertEquals();
+    }
+
+    @Test
+    public void testNewRoue3() {
+        assertEquals();
+    }
+
+    // Tests setters
+    @Test
+    public void testSetVoiture() {
+        assertEquals();
+    }
+
+    @Test
+    public void testSetDiametre() {
+        assertEquals();
+    }
+
+    @Test
+    public void testSetPoids() {
+        assertEquals();
+    }
+
+    // Tests associations
+
+    // Tests other methods : nada ?
+
+    // Tests exceptions
+    @Test
+    public void testSetVoitureException() {
+        assertThrows();
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
--- src/test/java/VoitureTest.java
+++ src/test/java/VoitureTest.java
@@ -40,14 +40,89 @@ public class VoitureTest {
     public void tearDown() {
     }
 
+    // Tests getters
+    @Test
+    public void testGetMarque() {
+        assertEquals();
+    }
+
+    @Test
+    public void testGetCouleur() {
+        assertEquals();
+    }
+
+    @Test
+    public void testGetCarrosserie() {
+        assertEquals();
+    }
+
+    @Test
+    public void testGetRoues() {
+        assertEquals();
+    }
+
+    // Tests constructors
+    @Test
+    public void testNewVoiture1(){
+
+    }
+
+    @Test
+    public void testNewVoiture2(){
+
+    }
+
+    @Test
+    public void testNewVoiture3(){
+
+    }
+
+    // Tests setters
+    @Test
+    public void testSetMarque(){
+
+    }
+
+    @Test
+    public void testSetCouleur(){
+
+    }
+
+    @Test
+    public void testSetCarrosserie(){
+
+    }
+
+    @Test
+    public void testSetRoues(){
+
+    }
+
+    // Tests associations
+
+
+    // Tests autres méthodes
+    @Test
+    public void testGetPoids() {
+        assertEquals(1000, voiture1.getPoids());
+    }
+
+    @Test
+    public void testAddRoue() {
+
+    }
+
+    // Tests exceptions
+    @Test
+    public void testAddRoueException() {
+        assertThrows();
+    }
+
     @Test
     public void testToString() {
         assertEquals("Rolls Royce de couleur noire", voiture1.toString());
     }
 
-    @Test
-    public void testGetPoids() {
-        assertEquals(1000, voiture1.getPoids());
-    }
+
 }
 
