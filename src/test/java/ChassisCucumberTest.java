9b0969131ebf7748258dec760338300c5da98b56
Perciii <qs.sauvage@gmail.com>
2020-05-22 11:49:02 +0200

    add chassis cucumber test skeleton

---
+++ src/test/java/ChassisCucumberTest.java
@@ -0,0 +1,21 @@
+public class ChassisCucumberTest {
+    @io.cucumber.java.en.Given("le client appelle le constructeur de chassis")
+    public void leClientAppelleLeConstructeurDeChassis() {
+    }
+
+    @io.cucumber.java.en.When("le client saisit un poids positif")
+    public void leClientSaisitUnPoidsPositif() {
+    }
+
+    @io.cucumber.java.en.Then("le chassis est créé avec le poids indiqué en entrée")
+    public void leChassisEstCrééAvecLePoidsIndiquéEnEntrée() {
+    }
+
+    @io.cucumber.java.en.When("le client saisit un poids strictement négatif")
+    public void leClientSaisitUnPoidsStrictementNégatif() {
+    }
+
+    @io.cucumber.java.en.Then("le chassis est créé avec un poids de {int}")
+    public void leChassisEstCrééAvecUnPoidsDe(int arg0) {
+    }
+}
