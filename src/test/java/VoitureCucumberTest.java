56f2ea5716b47862aa7ffcf5434cb26a05c69bc0
Perciii <qs.sauvage@gmail.com>
2020-05-23 14:43:46 +0200

    add scenario outline for voiture

--- src/test/java/VoitureCucumberTest.java
+++ src/test/java/VoitureCucumberTest.java
@@ -1,3 +1,4 @@
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,7 +13,7 @@ public class VoitureCucumberTest {
     private ArrayList<Roue> roueList = new ArrayList<>();
 
     @Given("le client crée {int} roues de poids {int}, un chassis de poids {int}")
-    public void leClientCreeUnNouveauChassisSansSpecifierSonPoids(int nbRoues, int poidsRoue, int poidsChassis) {
+    public void leClientCreeDesRouesEtUnChassis(int nbRoues, int poidsRoue, int poidsChassis) {
         chassis = new Chassis(poidsChassis);
         for (int i = 0; i < nbRoues; i++) {
             roueList.add(new Roue(20, poidsRoue));
@@ -28,5 +29,34 @@ public class VoitureCucumberTest {
     @Then("Le poids de la voiture vaut {int}")
     public void verificationDuPoidsDeLaVoiture(int poidsVoiture) {
         assertEquals(poidsVoiture, voiture.getPoids());
+    }
+
+    @Given("le client a une voiture à {int} roues de poids {int}")
+    public void leClientAUneVoitureAvecDesRoues(int nbRoues, int poidsRoue) {
+        Chassis chassis = new Chassis(0);
+        voiture.setChassis(chassis);
+        roueList = new ArrayList<>();
+        for (int i = 0; i < nbRoues; i++) {
+            roueList.add(new Roue(20, poidsRoue));
+        }
+        voiture.setRoues(roueList);
+    }
+
+    @When("le client remplace une roue de sa voiture par une nouvelle de poids {int}")
+    public void leClientRemplaceUneRoue(int poidsRoue) {
+        Roue roueCassee = voiture.getRoues().get(0);
+        voiture.removeRoue(roueCassee);
+        Roue roueNeuve = new Roue(20, poidsRoue);
+        voiture.addRoue(roueNeuve);
+    }
+
+    @Then("la voiture a toujours {int} roues")
+    public void laVoitureAToujoursAutantDeRoues(int nbRoues) {
+        assertEquals(nbRoues, voiture.getRoues().size());
+    }
+
+    @And("le poids de la voiture est mis à jour au poids {int}")
+    public void lePoidsDeLaVoitureEstMisAJour(int poidsFinal) {
+        assertEquals(poidsFinal, voiture.getPoids());
     }
 }
--- src/test/resources/voiture.feature
+++ src/test/resources/voiture.feature
@@ -5,7 +5,7 @@ Feature: Création de voiture et de ses éléments
   Je veux créer ma voiture comme je l'entend,
   Afin de pouvoir maîtriser le poids de celle-ci.
 
-  Scenario Outline: le client ajoute des roues à une voiture
+  Scenario Outline: le client ajoute des roues et un chassis à une voiture
     Given le client crée <nombre de roues> roues de poids <poids des roues>, un chassis de poids <poids du chassis>
     When le client applique les éléments à la voiture
     Then Le poids de la voiture vaut <poids total>
@@ -14,3 +14,14 @@ Feature: Création de voiture et de ses éléments
       | nombre de roues | poids des roues | poids du chassis | poids total |
       | 4               | 50              | 200              | 400         |
       | 4               | 100             | 500              | 900         |
+
+  Scenario Outline: Remplacement d'une roue de voiture
+    Given le client a une voiture à <nombre de roues> roues de poids <poids de chaque roue>
+    When le client remplace une roue de sa voiture par une nouvelle de poids <poids nouvelle roue>
+    Then la voiture a toujours <nombre de roues> roues
+    And le poids de la voiture est mis à jour au poids <poids final>
+
+    Examples:
+      | nombre de roues | poids de chaque roue | poids nouvelle roue | poids final |
+      | 4               | 5                    | 6                   | 21          |
+      | 3               | 10                   | 15                  | 35          |
