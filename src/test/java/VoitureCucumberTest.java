09a803f8e0ea1dc9c9ab0fa297e11a5ebec408e5
Perciii <qs.sauvage@gmail.com>
2020-06-02 22:48:51 +0200

    Revert "add scenario outline for voiture"
    This reverts commit 56f2ea57

--- src/test/java/VoitureCucumberTest.java
+++ src/test/java/VoitureCucumberTest.java
@@ -1,4 +1,3 @@
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,12 +7,12 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class VoitureCucumberTest {
-    private Voiture voitureTransformers = new Voiture("Mercedes", "rouge");
+    private Voiture voiture = new Voiture("Mercedes", "rouge");
     private Chassis chassis;
     private ArrayList<Roue> roueList = new ArrayList<>();
 
     @Given("le client crée {int} roues de poids {int}, un chassis de poids {int}")
-    public void leClientCreeDesRouesEtUnChassis(int nbRoues, int poidsRoue, int poidsChassis) {
+    public void leClientCreeUnNouveauChassisSansSpecifierSonPoids(int nbRoues, int poidsRoue, int poidsChassis) {
         chassis = new Chassis(poidsChassis);
         for (int i = 0; i < nbRoues; i++) {
             roueList.add(new Roue(20, poidsRoue));
@@ -22,41 +21,12 @@ public class VoitureCucumberTest {
 
     @When("le client applique les éléments à la voiture")
     public void leClientAppliqueLesElementsALaVoiture() {
-        voitureTransformers.setRoues(roueList);
-        voitureTransformers.setChassis(chassis);
+        voiture.setRoues(roueList);
+        voiture.setChassis(chassis);
     }
 
     @Then("Le poids de la voiture vaut {int}")
     public void verificationDuPoidsDeLaVoiture(int poidsVoiture) {
-        assertEquals(poidsVoiture, voitureTransformers.getPoids());
-    }
-
-    @Given("le client a une voiture à {int} roues de poids {int}")
-    public void leClientAUneVoitureAvecDesRoues(int nbRoues, int poidsRoue) {
-        Chassis chassis = new Chassis(0);
-        voitureTransformers.setChassis(chassis);
-        roueList = new ArrayList<>();
-        for (int i = 0; i < nbRoues; i++) {
-            roueList.add(new Roue(20, poidsRoue));
-        }
-        voitureTransformers.setRoues(roueList);
-    }
-
-    @When("le client remplace une roue de sa voiture par une nouvelle de poids {int}")
-    public void leClientRemplaceUneRoue(int poidsRoue) {
-        Roue roueCassee = voitureTransformers.getRoues().get(0);
-        voitureTransformers.removeRoue(roueCassee);
-        Roue roueNeuve = new Roue(20, poidsRoue);
-        voitureTransformers.addRoue(roueNeuve);
-    }
-
-    @Then("la voiture a toujours {int} roues")
-    public void laVoitureAToujoursAutantDeRoues(int nbRoues) {
-        assertEquals(nbRoues, voitureTransformers.getRoues().size());
-    }
-
-    @And("le poids de la voiture est mis à jour au poids {int}")
-    public void lePoidsDeLaVoitureEstMisAJour(int poidsFinal) {
-        assertEquals(poidsFinal, voitureTransformers.getPoids());
+        assertEquals(poidsVoiture, voiture.getPoids());
     }
 }
